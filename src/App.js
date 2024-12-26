a8fe1e75f16f9b528c1e53b92752ca9d93378f68
seyeonsim <seyeonsimw@gmail.com>
2024-12-26 13:18:14 +0900

    feat: 임시 헤더 생성, 적용

--- src/App.js
+++ src/App.js
@@ -1,6 +1,16 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Header from "./components/Header";
+
 function App() {
   return (
-    <></>
+    <>
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route />
+      </Routes>
+    </BrowserRouter>
+    </>
   );
 }
 
