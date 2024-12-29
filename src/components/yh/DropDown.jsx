094fe31c504d098937ed39566a40dd8437425ed2
yuthewave <yuthewave@gmail.com>
2024-12-29 21:55:29 +0900

    작업 내용

--- src/components/yh/DropDown.jsx
+++ src/components/yh/DropDown.jsx
@@ -1,8 +1,12 @@
 import React from "react";
 
-const DropDown = ({ districts, onSelect }) => {
+const DropDown = ({ districts, onSelect, value }) => {
   return (
-    <select onChange={(e) => onSelect(e.target.value)} style={{ padding: "10px", marginBottom: "20px" }}>
+    <select
+      onChange={(e) => onSelect(e.target.value)}
+      value={value} // 선택된 값과 동기화
+      style={{ padding: "10px", marginBottom: "20px" }}
+    >
       <option value="">전체 지역</option>
       {districts.map((district, index) => (
         <option key={index} value={district}>
