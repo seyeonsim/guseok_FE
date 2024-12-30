146e4cbd5814c52221115c60abad6a2d297badd5
seyeonsim <seyeonsimw@gmail.com>
2024-12-30 09:34:10 +0900

    feat: 자치구별 문화행사 목록 조회 및 지도 변경

--- src/pages/CulturalEvent.jsx
+++ src/pages/CulturalEvent.jsx
@@ -5,7 +5,20 @@ import List from "../components/sy/List";
 
 function CulturalEvent() {
     const [district, setDistrict] = useState("중구");
+    const [districts, setDistricts] = useState([]);
     const [event, setEvent] = useState([]);
+    const [districtCoordinates, setDistrictCoordinates] = useState({ lat: 37.5637, lot: 126.9976 }); // 중구 기본값
+
+
+    const getDistricts = async () => {
+        try {
+          const response = await axios.get(process.env.REACT_APP_BACKSERVER +'/districts');
+          console.log(response.data);
+          setDistricts(response.data);
+        } catch (error) {
+          console.error(error);
+        }
+      };
 
     const getEventList = async () => {
         try {
@@ -22,21 +35,43 @@ function CulturalEvent() {
 
     };
 
-    // useEffect(() => {
-    //     getEventList();
-    // }, [])
-
     useEffect(() => {
         getEventList();
-    }, [district])
+        getDistricts();
+    }, [district]);
+
+    const handleDistrictChange = (e) => {
+        // console.log(e.target.value);
+        const selectedDistrict = e.target.value;
+        setDistrict(selectedDistrict);
+
+        // 선택된 자치구에 맞는 lat, lot 값을 찾기
+        const districtData = districts.find((item) => item.name === selectedDistrict);
+        if (districtData) {
+            setDistrictCoordinates({
+                lat: districtData.lat,
+                lot: districtData.lot
+            });
+        }
+
+    };
 
 
     return (
         <>
         <h1>Cultural Event</h1>
+        <h2>서울특별시 {district}</h2>
+        <select name="" id="" onChange={handleDistrictChange}>
+            <option value="">- 자치구 변경 -</option>
+            {districts.map((item) => (
+                <option key={item.id} value={item.name} >
+                    {item.name}
+                </option>
+            ))}
+        </select>
         <div style={{display: "flex"}}>
             <List event={event} />
-            <Map event={event} />
+            <Map event={event} districtCoordinates={districtCoordinates}/>
         </div>
         </>
     );
